10ad289112b60f39f7955c67d71023043340c1c1
yigalgru <yigalgru@walla.co.il>
2019-07-31 00:22:46 +0300

    hh

---
+++ WebApplication2/Controllers/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class Class1 : ApiController
+    {
+    }
+}
